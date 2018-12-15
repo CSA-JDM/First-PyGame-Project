20d3e08a05810aaa47a8a7b8f143eeabe8128327
Jacob Meadows <2178@chaparralstaracademy.com>
2018-12-15 16:39:34 -0600

    Initial commit

---
+++ First-PyGame-Project.py
@@ -0,0 +1,37 @@
+# Jacob Meadows
+# Computer Programming, 6th Period
+# 15 December 2018
+"""
+First-PyGame-Project.py
+
+My first PyGame project.
+
+Copyright (C) 2018 Jacob Meadows
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import pygame
+
+
+class App:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+if __name__ == '__main__':
+    App()
